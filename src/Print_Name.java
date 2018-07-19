774c3150d403777f56e29ffe6ad0e50201ef2107
Phillip Byrd <Phill.e.byrd@gmail.com>
2018-07-19 16:36:45 -0700

    Added a new java spell out to test the repo from desktop

--- src/Print_Name.java
+++ src/Print_Name.java
@@ -3,5 +3,7 @@ public class Print_Name {
 
         System.out.println("my Name is phillip");
         System.out.println("This is the second time I will tell you");
+        System.out.println("This is the third time I will tell you");
+
     }
 }
---
+++ src/Spellout_JAVA.java
@@ -0,0 +1,6 @@
+public class Spellout_JAVA {
+
+    public static void main (String[] args) {
+        System.out.println("JAVA!!!!!!!!!!");
+    }
+}
