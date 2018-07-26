c78085c8c8dd0bcecab89912bac7ca65cf2462e0
Phillip Byrd <Phill.e.byrd@gmail.com>
2018-07-26 13:58:03 -0700

    Started the google interview problems

---
+++ src/GoogleClock.java
@@ -0,0 +1,23 @@
+//Given a string S
+    //Non empty HH:MM
+    //What was the last time that could be displayed using the same subset of digits
+
+//make a main
+    //make a test string and comment an expected
+    //make an object
+    //use object to call function and pass the string
+
+//make a function that takes in a string and returns a string that is a new combination of the input or ""
+    //make an output variable
+    //if the string is null or empty
+        //return ""
+
+    //replace the ":"
+
+    //step through the string and
+
+
+
+
+public class GoogleClock {
+}
